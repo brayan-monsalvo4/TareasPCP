66cc942bc5dc235793a02e123b63e31a04671f8a
Brayan Monsalvo <brayanmonsalvo4@gmail.com>
2023-03-06 21:39:20 -0600

    run modificado: si al hijo no le toco poyo mandara un mensaje en consola y no se bloqueara el hilo

--- Actividad6_2/Hijo.java
+++ Actividad6_2/Hijo.java
@@ -8,15 +8,27 @@ public class Hijo extends Thread implements Comparable<Hijo>{
     public Hijo(int fechaNacimiento, Stack<String> piezasPollo){
         this.fechaNacimiento = fechaNacimiento;
         this.piezasPoyo = piezasPollo;
+        setName("hilo 1");
     }
 
     @Override
     public void run(){
-        System.out.println(fechaNacimiento+" tomo "+piezasPoyo.pop());
+        boolean ok = false;
+        try{
+            System.out.println(fechaNacimiento+" tomo "+piezasPoyo.pop());
+            ok = true;
+        }catch(Exception e){
+            ok = false;
+        }
+
+        if(!ok) System.out.println("el hijo "+getName()+"se quedo sin poyo");
     }
 
     @Override
     public int compareTo(Hijo hermano) {
         return Integer.compare(this.fechaNacimiento, hermano.fechaNacimiento);
     }
+
+
+    
 }
